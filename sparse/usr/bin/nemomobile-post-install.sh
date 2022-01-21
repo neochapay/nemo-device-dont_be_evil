c86414da194fee5e1fe1f76a17c6bc6cb66c9160
Chupligin Sergey <neochapay@gmail.com>
2022-01-21 09:20:07 +0000

    Add post install service

---
+++ sparse/usr/bin/nemomobile-post-install.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is the nemomobile post install script for PinePhone devices!
+gpasswd -a "manjaro" autologin
+
+# Disable this service, so it only gets run on first boot
+systemctl disable nemomobile-post-install.service
+
+# reboot phone
+reboot
