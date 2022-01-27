909fc5247bd9e6315cdaec1ef44c25fdc307c099
Chupligin Sergey <neochapay@gmail.com>
2022-01-27 16:52:23 +0300

    Fixup get mac address

--- sparse/usr/bin/droid-get-bt-address.sh
+++ sparse/usr/bin/droid-get-bt-address.sh
@@ -4,7 +4,7 @@ echo "droid-get-bt-address: Setting up bluetooth address"
 addresspath="/var/lib/bluetooth/"
 addressfile="$addresspath/board-address"
 
-mac=$(hciconfig | grep Address | grep -o "[[:xdigit:]:]\{11,17\}")
+mac=$(bluetoothctl list | grep default | grep -o "[[:xdigit:]:]\{11,17\}")
 if [ ! -f "$addressfile" ] && [ ! -z "$mac" ]; then
     echo "File not found, saving address"
     mkdir -p "$addresspath"
